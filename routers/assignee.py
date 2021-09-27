b33426ed1e9240d597da002bcd3c8b41c432c9dd
Yutarotaro <9343783873a@g.ecc.u-tokyo.ac.jp>
2021-09-27 14:18:52 +0900

    create dockerenv

---
+++ routers/assignee.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+#assignee
+@router.get("/assignee")
+async def assignee():
+	pass
+
+@router.post("/assignee")
+async def assignee():
+	pass
+
+@router.delete("/assignee")
+async def assignee():
+	pass
+
+@router.put("/assignee")
+async def assignee():
+	pass
+
+
