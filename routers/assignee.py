f9c87a3ecd1ef0c7e3762a7dc398779308696dcb
Yutarotaro <9343783873a@g.ecc.u-tokyo.ac.jp>
2021-09-29 13:53:10 +0900

    modify endpoint settings

--- routers/assignee.py
+++ routers/assignee.py
@@ -1,3 +1,4 @@
+from abc import get_cache_token
 from fastapi import APIRouter, Depends, HTTPException
 from typing import List,Tuple, Optional
 import api.schemas.assignee as assignee_schema
@@ -6,10 +7,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 import api.cruds.assignee as assignee_crud
 from api.db import get_db
 
-router = APIRouter()
+router = APIRouter(
+	prefix="/assignee/v1",
+	tags=["assignee/v1"],
+	responses={404: {"descriptoin": "Not found"}}
+)
 
 
-@router.get("/assignee", response_model=List[assignee_schema.AssigneeResponse])
+@router.get("/", response_model=List[assignee_schema.AssigneeResponse])
 async def get_all_assignees(db: AsyncSession = Depends(get_db)):
 	assignees = await assignee_crud.get_all_assignees(db)
 	if assignees is None:
@@ -18,7 +23,7 @@ async def get_all_assignees(db: AsyncSession = Depends(get_db)):
 	return assignees
 
 
-@router.get("/assignee/{assignee_id}", response_model=Optional[assignee_schema.AssigneeResponse])
+@router.get("/{assignee_id}", response_model=Optional[assignee_schema.AssigneeResponse])
 async def get_assignee(assignee_id: int, db: AsyncSession = Depends(get_db)):
 	assignee = await assignee_crud.get_assignee(db, assignee_id = assignee_id )
 	if assignee is None:
@@ -26,7 +31,7 @@ async def get_assignee(assignee_id: int, db: AsyncSession = Depends(get_db)):
 
 	return assignee
 
-@router.post("/assignee/", response_model=assignee_schema.AssigneeResponse)
+@router.post("/", response_model=assignee_schema.AssigneeResponse)
 async def add_assignee(assignee_body: assignee_schema.Assignee, db: AsyncSession = Depends(get_db)):
 	assignee =  await assignee_crud.add_assignee(db, assignee_body)
 	if assignee is None:
@@ -37,7 +42,7 @@ async def add_assignee(assignee_body: assignee_schema.Assignee, db: AsyncSession
 
 
 
-@router.delete("/assignee/{assignee_id}", response_model=str)
+@router.delete("/{assignee_id}", response_model=str)
 async def delete_assignee(assignee_id:int , db: AsyncSession = Depends(get_db)):
 	assignee = await assignee_crud.get_assignee(db, assignee_id = assignee_id)
 
@@ -50,7 +55,7 @@ async def delete_assignee(assignee_id:int , db: AsyncSession = Depends(get_db)):
 
 
 
-@router.put("/assignee/{assignee_id}", response_model=assignee_schema.AssigneeResponse)
+@router.put("/{assignee_id}", response_model=assignee_schema.AssigneeResponse)
 async def update_assignee(assignee_id: int, assignee_body: assignee_schema.Assignee, db: AsyncSession = Depends(get_db)):
 	assignee = await assignee_crud.get_assignee(db, assignee_id=assignee_id)
 
