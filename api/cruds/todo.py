e439d638b792fb082c868061e31b30b0a288e723
Yutarotaro <9343783873a@g.ecc.u-tokyo.ac.jp>
2021-09-30 11:19:18 +0900

    datetime.datetime.now()がJSTで動作するようにconfig.pyに設定した

--- api/cruds/todo.py
+++ api/cruds/todo.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 from sqlalchemy.sql.elements import Null
 from sqlalchemy.sql.expression import null
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -9,23 +9,22 @@ from typing import List, Tuple, Optional
 import api.models.model as todo_model
 import api.schemas.todo as todo_schema
 from sqlalchemy.sql.type_api import NULLTYPE
+import config
 
 
 
 #post
-async def add_todo(db: AsyncSession, add_schema: todo_schema.TodoResponse)->todo_model.Todo:
-    todo = todo_model.Todo(
-		content=add_schema.content,
-		deadline=add_schema.deadline,
-		is_done=add_schema.is_done,
-		created_at=add_schema.created_at,
-		updated_at=add_schema.updated_at,
-		deleted_at=add_schema.deleted_at
-		)
-    db.add(todo)
-    await db.commit()
-    await db.refresh(todo)
-    return todo
+async def add_todo(db: AsyncSession, add_schema: todo_schema.Todo)->todo_model.Todo:
+	todo = todo_model.Todo(
+				content=add_schema.content,
+				deadline=add_schema.deadline,
+				is_done=add_schema.is_done,
+				created_at= datetime.datetime.now(config.JST)
+				)
+	db.add(todo)
+	await db.commit()
+	await db.refresh(todo)
+	return todo
 
 
 #get all todos
@@ -61,15 +60,15 @@ async def update_todo(
 	db: AsyncSession, todo_schema: todo_schema.Todo, original:todo_model.Todo)-> todo_model.Todo:
 
 	if (not original.is_done) and todo_schema.is_done:    #終了したとき
-		original.deleted_at = datetime.now()
+		original.deleted_at = datetime.now(config.JST)
 	elif original.is_done and (not todo_schema.is_done):  #終了を取り消すとき
 		original.deleted_at = null
 
 	original.content = todo_schema.content if todo_schema.content else original.content
 	original.deadline = todo_schema.deadline if todo_schema.deadline else original.deadline
 	original.is_done = todo_schema.is_done if todo_schema.is_done else original.is_done
-	original.created_at = todo_schema.created_at if todo_schema.created_at else original.created_at
-	original.updated_at = datetime.now()
+	#original.created_atは変更しない
+	original.updated_at = datetime.now(config.JST)
 
 	db.add(original)
 	await db.commit()
