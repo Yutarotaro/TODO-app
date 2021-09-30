fe1e32a857d101a097e8590c146a8a56dcd2215e
Yutarotaro <9343783873a@g.ecc.u-tokyo.ac.jp>
2021-09-30 11:20:35 +0900

    postとputにcreated_at, updated_at, deleted_atが必要ないと思い、schemaを変更した

--- api/schemas/todo.py
+++ api/schemas/todo.py
@@ -6,12 +6,13 @@ class Todo(BaseModel):
 	content: str = Field(...)
 	deadline: datetime.datetime = Field(...)
 	is_done: bool = Field(False)
-	created_at: Optional[datetime.datetime] = Field(...)
-	updated_at: Optional[datetime.datetime] = Field(datetime.datetime.now())
-	deleted_at: Optional[datetime.datetime] = Field(...)
+
 
 class TodoResponse(Todo):
 	todo_id: int
+	created_at: Optional[datetime.datetime] = Field(...)
+	updated_at: Optional[datetime.datetime] = Field(...)
+	deleted_at: Optional[datetime.datetime] = Field(...)
 
 	class Config:
 		orm_mode = True
